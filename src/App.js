11bd31814a88f083976e74df5de27714ee950f94
Pwinvit Boonsri <bo.bwinvit_st@tni.ac.th>
2022-07-30 18:43:17 +0700

    add display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
-function App() {
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Calculator = styled.div`
+  max-width: 700px;
+  max-height: 840px;
+  width: 50vw;
+  height: 60vh;
+  background-color: white;
+  border-radius: 3%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  /* align-items: center; */
+`
+
+const Display = styled.div`
+  margin: 5%;
+  width: 90%;
+  height: 30%;
+  background-color: black;
+`
+
+const CalculatorGroupButton = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  height: 70%;
+`
+
+const CalculatorButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0.2px solid grey;
+`
+
+const buttonArray = [
+  '7', '8', '9', '/',
+  '4', '5', '6', 'x',
+  '1', '2', '3', '-',
+  '.', '0', '=', '+',
+]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <Calculator>
+        <Display></Display>
+        <CalculatorGroupButton>
+          {buttonArray.map(eachButton => {
+            return (
+              <CalculatorButton>{eachButton}</CalculatorButton>
+            )
+          })}
+        </CalculatorGroupButton>
+      </Calculator>
+    </Wrapper>
+  )
 }
 
 export default App;
